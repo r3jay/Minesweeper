a9dc97d4bbe0b6bd560f688490a48175cda9e61d
Fratley <jose.mate.11@gmail.com>
2022-06-08 23:14:47 -0400

    Add project files.

---
+++ cMain.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "wx/wx.h"
+
+class cMain : public wxFrame
+{
+public:
+	cMain();
+	~cMain();
+
+	/*wxButton *m_btn1 = nullptr;
+	wxTextCtrl* m_txt1 = nullptr;
+	wxListBox* m_list1 = nullptr;*/
+
+	int nFieldWidth = 10;
+	int nFieldHeight = 10;
+	wxButton** btn;
+	int* nField = nullptr;
+	bool bFirstClick = true;
+
+
+	void OnButtonClicked(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
+};
+
